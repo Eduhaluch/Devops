0dabb48a4e5040c3d58046dc92c84083969d2a85
Eduhaluch <103967764+Eduhaluch@users.noreply.github.com>
2024-09-29 15:41:18 -0300

    Atualização semana 8

--- test/tests.py
+++ test/tests.py
@@ -5,15 +5,19 @@ from unittest.mock import patch
 def test_funcao_teste():
     with patch('random.randint', return_value=12345):
         result = funcao_teste()
-
+        yield result
     assert result == {"teste": True, "num_aleatorio": 12345}
 
 
 
 def root_test():
+    result = root_test()
+    yield result
     assert root() == {"message": "Hello World"}
 
 
 
 def deucerto_test():
+    result = deucerto_test()
+    yield result
     assert deucerto() == {"teste": "se viu é pq deu certo!"}
